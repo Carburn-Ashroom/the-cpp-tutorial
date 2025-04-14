109cef68c41b54e59858c68b3555774dc4d117f1
Carburn Ashroom <2257763704@qq.com>
2025-04-14 11:06:52 +0800

    前20天

---
+++ Day 6/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    int ji = 2.5;
+    int niu, yang;
+    int ma {7};
+    ma + 7;
+    if (ma = 14) {
+        ji = niu;
+        printf("%lf", ji);
+    }
+	return 0;
+}
